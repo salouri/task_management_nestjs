16de8c18c9d5518935b66b5148747551d07b66c9
Saeed Badran <saeed.badran@pixelandtheory.com>
2024-01-17 15:59:02 +0300

    tasks service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {
     this.tasksService = tasksService;
+  }
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Body('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(
+    // @Body('title') title: string,
+    // @Body('description') description: string, // Old way
+    @Body() createTaskDto: CreateTaskDto,
+  ): Task {
+    const task = this.tasksService.createTask(createTaskDto);
+    return task;
   }
 }
