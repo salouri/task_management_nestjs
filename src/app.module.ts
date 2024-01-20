c179eb47c914d788095a65659d81df074a086ff6
Saeed Badran <saeed.badran@pixelandtheory.com>
2024-01-21 00:14:43 +0300

    use ConfigService globally for env vars (from .env)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
