76e6a3b1746ad3e416a3eda1656784fe504131e4
Saeed Badran <saeed.badran@pixelandtheory.com>
2024-01-22 04:38:32 +0300

    use typeorm package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    TasksModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass1234',
+      database: 'tasks_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
