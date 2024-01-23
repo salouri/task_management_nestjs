2af7a3841180daeeec173cf08b343db0962c41e2
Saeed Badran <saeed.badran@pixelandtheory.com>
2024-01-23 22:35:55 +0300

    mminor modifs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
-import { User } from 'src/users/entity/user.entity';
 import { SigninUserDto } from 'src/users/dto/signin-user.dto';
 
 @Controller({
@@ -12,7 +11,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  createUser(@Body() data: CreateUserDto): Promise<{ id: string }> {
+  signUp(@Body() data: CreateUserDto): Promise<{ id: string }> {
     return this.authService.signUp(data);
   }
 
