05ce3ae5172d6374b80e3fd51faeef58eb1ba8d5
Saeed Badran <saeed.badran@pixelandtheory.com>
2024-01-28 05:42:04 +0300

    JWT Authentication: add routes (refresh, signout) with Guards and decorators for public APIs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { SigninUserDto } from 'src/users/dto/signin-user.dto';
+import { Tokens } from './types/tokens.type';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { User } from 'src/users/entity/user.entity';
+import { RefreshPayloadType } from './types/refresh-payload.type';
+import { RefreshJwtGuard } from './guards/rt.guard';
+import { PublicApi } from './decorator/public-api.decorator';
 
 @Controller({
   path: 'auth',
@@ -10,13 +16,29 @@ import { SigninUserDto } from 'src/users/dto/signin-user.dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @PublicApi()
   @Post('signup')
-  signUp(@Body() data: CreateUserDto): Promise<{ id: string }> {
-    return this.authService.signUp(data);
+  async signUp(@Body() data: CreateUserDto): Promise<Tokens> {
+    return await this.authService.signUp(data);
   }
 
+  @PublicApi()
   @Post('signin')
-  signIn(@Body() data: SigninUserDto): Promise<{ access_token: string }> {
-    return this.authService.signIn(data);
+  async signIn(@Body() data: SigninUserDto): Promise<Tokens> {
+    return await this.authService.signIn(data);
+  }
+
+  // @UseGuards(AccessJwtGuard)
+  @Post('signout')
+  async signOut(@GetUser() user: User): Promise<void> {
+    return await this.authService.signOut(user.id);
+  }
+
+  @PublicApi() // to bypass the global AuthGuard('jwt')
+  @UseGuards(RefreshJwtGuard)
+  @Post('refresh')
+  async refreshTokens(@GetUser() payload: RefreshPayloadType): Promise<Tokens> {
+    const { email, refreshToken } = payload;
+    return await this.authService.refreshTokens(email, refreshToken);
   }
 }
