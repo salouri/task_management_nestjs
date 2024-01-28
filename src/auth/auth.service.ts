6f2af3e20f0c1f08e49d91ee558856660dd472f7
Saeed Badran <saeed.badran@pixelandtheory.com>
2024-01-28 05:38:15 +0300

    JWT Authentication: add methods: hashData, signup, signout, updateRtHash, signTokens and refreshTokens

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
@@ -8,6 +12,7 @@ import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { User } from 'src/users/entity/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { SigninUserDto } from 'src/users/dto/signin-user.dto';
+import { Tokens } from './types/tokens.type';
 
 @Injectable()
 export class AuthService {
@@ -18,42 +23,77 @@ export class AuthService {
     private jwt: JwtService,
   ) {}
 
-  async signUp(data: CreateUserDto): Promise<{ id: string }> {
-    return this.usersService.create(data);
+  async hashData(data: string): Promise<string> {
+    return await argon.hash(data);
   }
 
-  async signIn(data: SigninUserDto): Promise<{ access_token: string }> {
+  async signUp(data: CreateUserDto): Promise<Tokens> {
+    const user = await this.usersService.create(data);
+    const tokens = await this.signTokens(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
+    return tokens;
+  }
+
+  async signIn(data: SigninUserDto): Promise<Tokens> {
     const { email, password } = data;
     try {
       const user = await this.usersService.findByEmail(email);
       const pwMatches = await argon.verify(user.password, password);
       if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
 
-      const token = await this.singToken(user.id, email);
-      return token;
+      const tokens = await this.signTokens(user.id, email);
+      await this.updateRtHash(user.id, tokens.refresh_token);
+
+      return tokens;
     } catch (error) {
       throw new UnauthorizedException('Credentials incorrect');
     }
   }
 
-  async singToken(
-    userId: string,
-    email: string,
-  ): Promise<{ access_token: string }> {
+  async signOut(userId: string): Promise<void> {
+    await this.usersService.updateRtHash(userId);
+  }
+
+  async refreshTokens(email: string, rt: string): Promise<Tokens> {
+    try {
+      const user = await this.usersService.findByEmail(email);
+      if (!user || !user.hashedRt)
+        throw new ForbiddenException('Access Denied');
+
+      const rtMatches = await argon.verify(user.hashedRt, rt);
+      if (!rtMatches) throw new ForbiddenException('Access Denied');
+
+      const tokens = await this.signTokens(user.id, email);
+
+      await this.updateRtHash(user.id, tokens.refresh_token);
+      return tokens;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async signTokens(id: string, email: string): Promise<Tokens> {
     const payload = {
-      sub: userId,
+      id,
       email,
     };
-    const expTimeMin = this.config.get('JWT_EXPIRES_IN');
-    const jwtSecret = this.config.get('JWT_SECRET');
 
-    const access_token = await this.jwt.signAsync(payload, {
-      expiresIn: expTimeMin,
-      secret: jwtSecret,
-    });
+    // expiresIn: 60, "2 days", "10h", "7d". A numeric value is interpreted as a seconds count.
+    const [at, rt] = await Promise.all([
+      this.jwt.signAsync(payload, {
+        expiresIn: this.config.get('ACCESS_JWT_EXPIRES_IN'),
+        secret: this.config.get('ACCESS_JWT_SECRET'),
+      }),
+      this.jwt.signAsync(payload, {
+        expiresIn: this.config.get('REFRESH_JWT_EXPIRES_IN'),
+        secret: this.config.get('REFRESH_JWT_SECRET'),
+      }),
+    ]);
+    return { access_token: at, refresh_token: rt };
+  }
 
-    return {
-      access_token,
-    };
+  async updateRtHash(userId: string, refreshToken: string): Promise<void> {
+    const hash = await this.hashData(refreshToken);
+    await this.usersService.updateRtHash(userId, hash);
   }
 }
