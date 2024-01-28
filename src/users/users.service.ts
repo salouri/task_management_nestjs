03606ae37e0a325312c76eaa8275ffcb3d0a4fcb
Saeed Badran <saeed.badran@pixelandtheory.com>
2024-01-28 05:45:45 +0300

    JWT Authentication: updateRtHash metho

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -15,12 +15,12 @@ export class UsersService {
     @InjectRepository(User) private readonly userRepo: Repository<User>,
   ) {}
 
-  async create(data: CreateUserDto): Promise<{ id: string }> {
+  async create(data: CreateUserDto): Promise<{ id: string; email: string }> {
     const { email, password } = data;
     const user = this.userRepo.create({ email, password });
     try {
       await this.userRepo.save(user);
-      return { id: user.id };
+      return { id: user.id, email };
     } catch (error) {
       if (error.code === '23505')
         throw new ConflictException('email already exists!');
@@ -33,5 +33,9 @@ export class UsersService {
     if (!user)
       throw new NotFoundException(`User with email '${email}' does not exist`);
     return user;
+  }
+
+  async updateRtHash(id: string, hash?: string): Promise<void> {
+    await this.userRepo.update(id, { hashedRt: hash || null });
   }
 }
